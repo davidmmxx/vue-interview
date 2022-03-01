8aceeab301c818e2f05f55c99ede40d92890db28
davidm <david.malec@external.t-mobile.cz>
2022-03-01 10:22:44 +0100

    initial cleanup

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,14 +1,26 @@
 <template>
   <div>
 {{msg}}
+    {{x}}
   </div>
 </template>
 
 <script>
+import {computed} from "vue";
+
 export default {
   name: 'HelloWorld',
   props: {
     msg: String
+  },
+  setup() {
+    const x = computed(() => {
+      return 'x';
+    })
+
+  return {
+    x
+  }
   }
 }
 </script>
